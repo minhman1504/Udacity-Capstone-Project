b35a9772e8d0ec3b94ed8e9f6a619706e5280d80
minhman1504 <minhman1504@gmail.com>
2022-05-25 10:06:24 +0700

    update api url

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://adb64a2643f7d45389f05585a243cd62-2002616473.ap-southeast-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
